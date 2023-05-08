b78781615a160f81ea64eb2c2aa090b1d8b8f916
ethan.repp <code@ethanrepp.com>
2023-05-08 09:31:52 -0700

    feat: add material styles

--- src/app/notify/notify.module.ts
+++ src/app/notify/notify.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import { CoreMaterialModule } from "../core-material.module";
+
 import { NotificationCardComponent } from "./notification-card/notification-card.component";
 import { NotifyRoutingModule } from "./notify-routing.module";
 
 @NgModule({
   declarations: [NotificationCardComponent],
-  imports: [CommonModule, NotifyRoutingModule],
+  imports: [CommonModule, CoreMaterialModule, NotifyRoutingModule],
 })
 export class NotifyModule {}
